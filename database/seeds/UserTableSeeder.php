3ca84485f3fcb8e9278d674b90f1af63f6300c4a
v123582 <v123582@gmail.com>
2015-11-07 02:09:15 +0800

    feature/dataSeeder git init: add userSeeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // DB::table('users')->insert([
+        //     'name' => str_random(10),
+        //     'email' => str_random(10).'@gmail.com',
+        //     'password' => bcrypt('secret'),
+        //     $table->timestamps();
+        // ]);
+        User::create(array(
+            'name' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ));
+    }
+}
